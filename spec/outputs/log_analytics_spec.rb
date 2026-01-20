7ad333d48bc81b72547f5ee5a43845d07c9dbc3d
oscar.moyeda@oracle.com <omoyeda@omoyeda.subnet1rg2phxsu.emdevinfraphx1.oraclevcn.com>
2026-01-20 19:30:22 +0000

    rename quick fix unit test

--- spec/outputs/log_analytics_spec.rb
+++ spec/outputs/log_analytics_spec.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
-require "logstash/outputs/logan"
+require "logstash/outputs/log_analytics"
 require "logstash/event"
 require "stringio"
 
